afe130782cfb9cc0b2df9a19df4e2c460361598d
jonathan <jonathanencina2020@gmail.com>
2025-09-18 22:43:54 -0500

    mas tematicas

--- client/src/components/WizardModal.tsx
+++ client/src/components/WizardModal.tsx
@@ -14,6 +14,8 @@ import pregnancyImg from "@assets/foto15_1758126876415.jpg";
 import familyImg from "@assets/48b44a95-4f4f-4662-bd86-7f0e518e6142_1758126876416.jpg";
 import birthdayImg from "@assets/542b0a32-3e11-4f3d-8691-a2a24c855bb8_1758126876419.jpg";
 import baptismImg from "@assets/2e4124f7-dc68-4be9-a6ab-e8684e3b707e_1758126876415.jpg";
+import baptismImg2 from "@assets/Bandas-de-graduación-personalizadas-cómo-pedirlas-en-tu-colegio.jpg";
+import baptismImg3 from "@assets/dasdsacacasc.jpg";
 
 import disneyImg from "@assets/91d832aa-62de-4a65-bfc6-f0054089cf7b_1758126876418.jpg";
 import marvelImg from "@assets/492330742_1240768438048772_8307688729786010659_n_1758126876417.jpg";
@@ -21,6 +23,9 @@ import halloweenImg from "@assets/6e962d61-7aa0-4010-9889-956b71e2e25e_175812687
 import christmasImg from "@assets/494147861_1296189768892720_4684710318659525456_n_1758126876418.jpg";
 import fantasyImg from "@assets/9c474195-07bc-4bdd-bdea-12067ed76364_1758126876414.jpg";
 import othersImg from "@assets/89c5c25d-1415-4cd8-b897-0ac5beb26d88_1758126876416.jpg";
+import fantasyImg2 from "@assets/425308320_890181756449018_2574818442999104143_n.jpg";
+
+
 
 interface WizardData {
   photoTypes: string[];
@@ -35,21 +40,24 @@ interface WizardModalProps {
 }
 
 const photoTypeOptions = [
-  { id: 'baby', label: 'Bebé', icon: '👶', sampleImg: babyImg },
-  { id: 'child', label: 'Niño', icon: '🧒', sampleImg: childImg },
+  { id: 'baby', label: 'Bebé (recien nacido)', icon: '👶', sampleImg: babyImg },
+  { id: 'child', label: 'Niño/a', icon: '🧒', sampleImg: childImg },
   { id: 'pregnancy', label: 'Embarazo', icon: '🤱', sampleImg: pregnancyImg },
   { id: 'family', label: 'Familiar', icon: '👨‍👩‍👧‍👦', sampleImg: familyImg },
   { id: 'birthday', label: 'Cumpleaños', icon: '🎂', sampleImg: birthdayImg },
   { id: 'baptism', label: 'Bautizo', icon: '⛪', sampleImg: baptismImg },
+  { id: 'baptism', label: 'Eliminar/agregar personas a una foto', icon: '-', sampleImg: baptismImg3 },
 ];
 
 const themeOptions = [
   { id: 'disney', label: 'Disney', icon: '🏰', sampleImg: disneyImg },
-  { id: 'marvel', label: 'Marvel', icon: '🦸', sampleImg: marvelImg },
+  { id: 'marvel', label: 'Superheroe', icon: '🦸', sampleImg: marvelImg },
   { id: 'halloween', label: 'Halloween', icon: '🎃', sampleImg: halloweenImg },
   { id: 'christmas', label: 'Navidad', icon: '🎄', sampleImg: christmasImg },
   { id: 'fantasy', label: 'Fantasía', icon: '🧚', sampleImg: fantasyImg },
+  { id: 'fantasy', label: 'Princesas', icon: '🧚', sampleImg: fantasyImg2 },
   { id: 'others', label: 'Otros', icon: '✨', sampleImg: othersImg },
+  { id: 'graduación', label: 'Graduación', icon: ':)', sampleImg: baptismImg2 },
 ];
 
 export default function WizardModal({ isOpen, onClose }: WizardModalProps) {
